2156a2ac986068794d1581026b9f22e9209e3f66
krypto-dev <kryptodev7@gmail.com>
2021-12-10 05:57:42 +1000

    Update S token

--- contracts/sBounty.sol
+++ contracts/sBounty.sol
@@ -558,7 +558,7 @@ contract StakedBounty is ERC20Permit, Ownable {
     uint public INDEX;
 
     uint256 private constant MAX_UINT256 = ~uint256(0);
-    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 10000 * 10**9;
+    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 50000 * 10**9;
 
     // TOTAL_GONS is a multiple of INITIAL_FRAGMENTS_SUPPLY so that _gonsPerFragment is an integer.
     // Use the highest value that fits in a uint256 for max granularity.
--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -8,7 +8,7 @@ import Loading from "../components/Loader";
 function Root() {
     const isApp = (): boolean => {
         // return window.location.host.includes("app");
-        return false;
+        return true;
     };
 
     const [loading, setLoading] = useState(true);
